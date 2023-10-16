32db234725a1f6934e91eaf8beab631b6542cb8f
RAIHAN ABDILLAH <30065695@tafe.wa.edu.au>
2023-10-16 11:47:34 +0800

    Add project files.

---
+++ MalinSpaceLogin-DictionaryData/GeneralGUI.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace MalinSpaceLogin_DictionaryData
+{
+    partial class GeneralGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.lbName = new System.Windows.Forms.Label();
+            this.lbID = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.gbLogin = new System.Windows.Forms.GroupBox();
+            this.gbShortcut = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gbLogin.SuspendLayout();
+            this.gbShortcut.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(54, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(196, 342);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(302, 210);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(196, 186);
+            this.listBox2.TabIndex = 1;
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(6, 28);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(60, 13);
+            this.lbName.TabIndex = 2;
+            this.lbName.Text = "Staff Name";
+            // 
+            // lbID
+            // 
+            this.lbID.AutoSize = true;
+            this.lbID.Location = new System.Drawing.Point(6, 66);
+            this.lbID.Name = "lbID";
+            this.lbID.Size = new System.Drawing.Size(43, 13);
+            this.lbID.TabIndex = 3;
+            this.lbID.Text = "Staff ID";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(80, 28);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 20);
+            this.tbName.TabIndex = 4;
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(80, 66);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(100, 20);
+            this.tbID.TabIndex = 5;
+            // 
+            // gbLogin
+            // 
+            this.gbLogin.Controls.Add(this.tbID);
+            this.gbLogin.Controls.Add(this.lbName);
+            this.gbLogin.Controls.Add(this.tbName);
+            this.gbLogin.Controls.Add(this.lbID);
+            this.gbLogin.Location = new System.Drawing.Point(302, 45);
+            this.gbLogin.Name = "gbLogin";
+            this.gbLogin.Size = new System.Drawing.Size(196, 115);
+            this.gbLogin.TabIndex = 6;
+            this.gbLogin.TabStop = false;
+            // 
+            // gbShortcut
+            // 
+            this.gbShortcut.Controls.Add(this.label3);
+            this.gbShortcut.Controls.Add(this.label2);
+            this.gbShortcut.Controls.Add(this.label1);
+            this.gbShortcut.Location = new System.Drawing.Point(549, 45);
+            this.gbShortcut.Name = "gbShortcut";
+            this.gbShortcut.Size = new System.Drawing.Size(200, 100);
+            this.gbShortcut.TabIndex = 7;
+            this.gbShortcut.TabStop = false;
+            this.gbShortcut.Text = "Shortcuts";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // GeneralGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbShortcut);
+            this.Controls.Add(this.gbLogin);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "GeneralGUI";
+            this.Text = "Malin Staff Details";
+            this.gbLogin.ResumeLayout(false);
+            this.gbLogin.PerformLayout();
+            this.gbShortcut.ResumeLayout(false);
+            this.gbShortcut.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbID;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.GroupBox gbLogin;
+        private System.Windows.Forms.GroupBox gbShortcut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MalinSpaceLogin-DictionaryData/GeneralGUI.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+
+namespace MalinSpaceLogin_DictionaryData
+{
+    public partial class GeneralGUI : Form
+    {
+        public GeneralGUI()
+        {
+            InitializeComponent();
+        }
+
+        //4.1.	Create a Dictionary data structure with a TKey of type integer and a TValue of type string,
+        //      name the new data structure “MasterFile”.
+        
+        Dictionary <int, string> MasterFile = new Dictionary <int, string>();
+
+
+        //4.2.	Create a method that will read the data from the.csv file into the Dictionary data structure when the GUI loads.
+
+
+        //4.3.	Create a method to display the Dictionary data into a non-selectable display only list box (ie read only).
+
+
+        //4.4.	Create a method to filter the Staff Name data from the Dictionary into a second filtered and selectable list box.
+        //      This method must use a text box input and update as each character is entered.
+        //      The list box must reflect the filtered data in real time.
+
+
+        //4.5.	Create a method to filter the Staff ID data from the Dictionary into the second filtered and selectable list box.
+        //      This method must use a text box input and update as each number is entered.
+        //      The list box must reflect the filtered data in real time.
+
+
+        //4.6.	Create a method for the Staff Name text box which will clear the contents and place the focus into the Staff Name text box.
+        //      Utilise a keyboard shortcut.
+
+
+        //4.7.	Create a method for the Staff ID text box which will clear the contents and place the focus into the text box.
+        //      Utilise a keyboard shortcut.
+
+
+        //4.8.	Create a method for the filtered and selectable list box which will populate the two text boxes
+        //      when a staff record is selected.Utilise the Tab and keyboard keys.
+
+
+        //4.9.	Create a method that will open the Admin GUI when the Alt + A keys are pressed.
+        //      Ensure the General GUI sends the currently selected Staff ID and Staff Name
+        //      to the Admin GUI for Update and Delete purposes and is opened as modal.
+        //      Create modified logic to open the Admin GUI to Create a new user when the Staff ID 77
+        //      and the Staff Name is empty.Read the appropriate criteria in the Admin GUI for further information.
+
+        //4.10.	Add suitable error trapping and user feedback via a status strip or similar to ensure a fully functional User Experience.
+        //      Make all methods private and ensure the Dictionary is static and public.
+
+
+        //4.11.	Ensure all code is adequately commented.Map the programming criteria and features to your code/methods
+        //      by adding comments above the method signatures.
+        //      Ensure your code is compliant with the CITEMS coding standards (refer http://www.citems.com.au/).
+
+
+
+    }
+}
